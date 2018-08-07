023caca0446c2f7170e4acec9f0d5fd89e63423a
zzmedu <1596792965@qq.com>
2018-08-07 17:21:26 +0800

    修改了配置文件中的注释

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#编译+部署order站点
+# 编译+部署order站点
 
-#需要配置如下参数
+# 需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
 
@@ -20,25 +20,32 @@ killTomcat()
       kill -9 $pid
     fi
 }
+# 进入order工程根目录
 cd $PROJ_PATH/order
+# 执行maven命令
 mvn clean install
 
 # 停tomcat
 killTomcat
 
-# 删除原有工程
+# 删除ROOT目录
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+# 删除Root.war文件
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
+# 删除order.war文件
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
+# 进入到tomcat的webapps目录
 cd $TOMCAT_APP_PATH/webapps/
+# 重命名order.war为ROOT.war
 mv order.war ROOT.war
 
-# 启动Tomcat
+# 加载tomcat路径
 cd $TOMCAT_APP_PATH/
+# 启动Tomcat
 sh bin/startup.sh
 
 
